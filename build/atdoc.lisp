8564a927c5ea7075456029b97bb4b68346b101a4
David Lichteblau <david@lichteblau.com>
2008-03-02 18:04:05 +0100

    documentation update

--- build/atdoc.lisp
+++ build/atdoc.lisp
@@ -1,7 +1,10 @@
 (asdf:operate 'asdf:load-op :cxml-stp)
 (asdf:operate 'asdf:load-op :atdoc)
-(atdoc:generate-documentation '(:cxml-stp)
-			      "/home/david/src/lisp/cxml-stp/doc/"
-			      :index-title "cxml-stp API reference"
-			      :heading "cxml-stp"
-			      :css "cxml-stp.css")
+(let* ((base (asdf:component-pathname (asdf:find-system :cxml-stp)))
+       (atdoc-directory (merge-pathnames "doc/" base)))
+  (ensure-directories-exist atdoc-directory)
+  (atdoc:generate-documentation '(:cxml-stp)
+				atdoc-directory
+				:index-title "cxml-stp API reference"
+				:heading "cxml-stp"
+				:css "cxml-stp.css"))
